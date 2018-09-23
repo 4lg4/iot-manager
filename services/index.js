b18a9f027d83013de62efe59c206e4dd89628c5f
Adriano Leal (alga) <akgleal@gmail.com>
2018-09-23 20:16:35 +1000

    save locally and display received data

--- services/index.js
+++ services/index.js
@@ -1,14 +1,26 @@
+const db = require('./db.json');
+
 module.exports = [
   {
     method: 'post',
     route: '/iot/:clientId/:id',
     service: async (req, res) => {
       const { clientId, id } = req.params;
-      const { message } = req.body;
 
       if (!clientId || !id) {
         return res.status(500).send('client id and iot id are required');
       }
+
+      if (typeof req.body === 'string') {
+        db.push({
+          clientId,
+          iotId: id,
+          message: req.body,
+        });
+      } else {
+        db.push(Object.assign({}, req.body, {clientId, iotId: id}));
+      }
+      // const { message } = req.body;
 
       // if (err) {
       //   return res.status(500).send(err);
@@ -16,6 +28,15 @@ module.exports = [
 
       res.send({
         data: 'OK',
+      });
+    },
+  },
+  {
+    method: 'get',
+    route: '/dashboard',
+    service: (req, res)=> {
+      res.send({
+        data: db,
       });
     },
   },
